72ad66270f3b4babcba91c8d2ba6c6126801af96
TheCreatorAMA <alexanderakiu@gmail.com>
2022-08-26 07:42:45 -0700

    feat: add functionality to read in command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // env::args() gets an iterator of the command line arguments passed to it and collecet()
+    // converts it into a collection that can be stored in a vector.
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Query string: '{}'\nFile path: '{}'", query, file_path);
 }
