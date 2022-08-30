af0776556befbbad2d406a9170f22d266a8f81c9
TheCreatorAMA <alexanderakiu@gmail.com>
2022-08-30 06:48:48 -0700

    feat: refactor getting cli arguments and split logic into its own function

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,30 @@ fn main() {
     // converts it into a collection that can be stored in a vector.
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config_params = parse_config(&args);
 
-    println!("Query string: '{}'\nFile path: '{}'", query, file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read this file.");
+    println!("Query string: '{}'\nFile path: '{}'", config_params.query, config_params.file_path);
+
+    let contents = fs::read_to_string(config_params.file_path).expect("Should have been able to read this file.");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    // The clone method below "clones" the string in args so the Config struct can take ownership
+    // of the string. This is a less "optimal" way but is much more simple. Avoids having to deal
+    // with ownership and lifetimes.
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config {
+        query,
+        file_path,
+    }
 }
