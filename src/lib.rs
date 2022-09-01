ee0f37199d8ba092e96fedfba60fccc7991ccb23
TheCreatorAMA <alexanderakiu@gmail.com>
2022-09-01 07:42:10 -0700

    feat: added tests for case insensitive search and finished main search function

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,29 @@
-use std::fs;
 use std::error::Error;
+use std::fs;
 
-pub fn run (config: Config) -> Result<(), Box<dyn Error>> {
-    let _contents = fs::read_to_string(config.file_path)?;
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+pub fn search_case_insensitive<'a>(_query: &str, _contents: &'a str) -> Vec<&'a str> {
     vec![]
 }
 
@@ -28,7 +44,7 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path})
+        Ok(Config { query, file_path })
     }
 }
 
@@ -37,13 +53,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let content = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, content));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, content)
+        );
     }
 }
